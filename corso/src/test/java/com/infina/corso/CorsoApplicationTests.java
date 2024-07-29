95b7fd2d65307f49a437dbb399921acf31b628a0
yunuskyya <0yunuskaya0@gmail.com>
2024-07-29 22:58:15 +0300

    proje oluşturuldu.

---
+++ corso/src/test/java/com/infina/corso/CorsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infina.corso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
