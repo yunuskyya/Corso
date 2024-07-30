6f07bed69426f1e2b95a078c81d03b0d15816ed4
yunuskyya <0yunuskaya0@gmail.com>
2024-07-30 19:54:54 +0300

    User entity sınıfına Token ve Role ilişkilendirmeleri eklendi.

--- corso/src/main/java/com/infina/corso/model/User.java
+++ corso/src/main/java/com/infina/corso/model/User.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.Set;
+
 @Data
 @Builder
 @Entity
@@ -22,5 +25,14 @@ public class User {
     private boolean active;
     private boolean isDeleted;
     private String activationToken;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Token> tokens;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> authorities;
 
 }
