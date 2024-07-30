764fa4e6cd845c804d78d2bd7f9538599cb3d9c2
yunuskyya <0yunuskaya0@gmail.com>
2024-07-30 22:13:22 +0300

    Kullanıcı kayıt işlemleri eklendi.

--- corso/src/main/java/com/infina/corso/repository/UserRepository.java
+++ corso/src/main/java/com/infina/corso/repository/UserRepository.java
@@ -9,5 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 
 }
