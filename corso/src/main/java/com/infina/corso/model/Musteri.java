66ff45be1def6a16f87d283a79240acc4b574085
nhtY <nihat.yalcin@agu.edu.tr>
2024-07-30 23:33:21 +0300

    Musteri modeli ve MusteriTur enum'ı oluşturuldu.

---
+++ corso/src/main/java/com/infina/corso/model/Musteri.java
@@ -0,0 +1,17 @@
+package com.infina.corso.model;
+
+import com.infina.corso.enums.MusteriTur;
+
+import java.util.List;
+
+public class Musteri {
+    private Long id;
+    private String name;
+    private String surname;
+    private MusteriTur musteriTur;
+    private String email;
+
+    // private List<Hesap> hesaplar;
+
+}
+
