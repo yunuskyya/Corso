eb326191e7da656ac81de2730d29b4d6cd154eb6
yunuskyya <0yunuskaya0@gmail.com>
2024-07-30 22:06:31 +0300

    ObjectMapper konfigürasyonu eklendi.

---
+++ corso/src/main/java/com/infina/corso/config/ObjectMapperConfig.java
@@ -0,0 +1,20 @@
+package com.infina.corso.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new ParameterNamesModule());
+        mapper.registerModule(new Hibernate5Module());
+        return mapper;
+    }
+}
