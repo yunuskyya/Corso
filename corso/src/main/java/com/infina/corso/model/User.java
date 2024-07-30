6f93af0f5c2df123ada5c6efe6276a63e0870e60
yunuskyya <0yunuskaya0@gmail.com>
2024-07-30 19:46:10 +0300

    user sınıfı güncellendi.

--- corso/src/main/java/com/infina/corso/model/User.java
+++ corso/src/main/java/com/infina/corso/model/User.java
@@ -1,18 +1,26 @@
 package com.infina.corso.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
-    private String firstName;
-    private String lastName;
     private String email;
     private String password;
     private boolean active;
     private boolean isDeleted;
     private String activationToken;
-    private String passwordResetToken;
-    private String profileDescription;
-
 
 }
