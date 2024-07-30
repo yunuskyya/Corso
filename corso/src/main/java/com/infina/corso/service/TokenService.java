5d6b4bdb0bafed165fae513a525d24b6cf7a309b
yunuskyya <0yunuskaya0@gmail.com>
2024-07-30 22:45:32 +0300

    TokenService ve TokenServiceImpl: token yönetimi ve doğrulama eklendi.

---
+++ corso/src/main/java/com/infina/corso/service/TokenService.java
@@ -0,0 +1,22 @@
+package com.infina.corso.service;
+
+import com.infina.corso.dto.request.CredentialsRequest;
+import com.infina.corso.dto.response.GetUserByEmailResponse;
+import com.infina.corso.model.Token;
+import com.infina.corso.model.User;
+
+public interface TokenService {
+    public Token generateToken(GetUserByEmailResponse user, CredentialsRequest credentials);
+    public User verifyToken(String authorizationHeader);
+
+    public void logout(String authorizationHeader);
+
+    public Token findToken(String cookieValue);
+
+    public void updateExpirationDate(Token token);
+
+    public void updateTokenUser(int userId);
+
+    public Token findTokenByUserId(int userId);
+
+}
