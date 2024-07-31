d4298a74e8f09e102e2a499dadd12c0be77bde4a
nhtY <nihat.yalcin@agu.edu.tr>
2024-07-31 15:08:26 +0300

    Account entity oluşturuldu.

---
+++ corso/src/main/java/com/infina/corso/model/Account.java
@@ -0,0 +1,38 @@
+package com.infina.corso.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String accountNumber;
+    private String iban;
+    private String bankName;
+    private String currency;
+    private BigDecimal balance;
+    private boolean isDeleted;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
