ba6676f130526c84fe174f7d63df3e3dcc71193a
yunuskyya <0yunuskaya0@gmail.com>
2024-08-01 21:27:10 +0300

    CredentialsRequest DTO'sundan username alanı çıkarıldı

--- corso/src/main/java/com/infina/corso/dto/request/CredentialsRequest.java
+++ corso/src/main/java/com/infina/corso/dto/request/CredentialsRequest.java
@@ -3,5 +3,5 @@ package com.infina.corso.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record CredentialsRequest(@Email String email, @NotBlank String password, String username) {
+public record CredentialsRequest(@Email String email, @NotBlank String password) {
 }
