3c7ac392db48ba3bb9a69b42297970b6c1d9e5ce
Erkal <erkal@example.com>
2024-08-01 13:54:09 +0300

    Repositoryden çekilen Transaction listesini TransactionDto listesine çevirme işlemi yapıldı

--- corso/src/main/java/com/infina/corso/service/impl/TransactionServiceImp.java
+++ corso/src/main/java/com/infina/corso/service/impl/TransactionServiceImp.java
@@ -8,6 +8,9 @@ import com.infina.corso.service.TransactionService;
 import com.infina.corso.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class TransactionServiceImp implements TransactionService {
 
@@ -31,6 +34,26 @@ public class TransactionServiceImp implements TransactionService {
         //userService.setTransaction(transaction); //transactionu usere kaydedecek ? ya da Customera kaydedecek ?
         transactionRepository.save(transaction);
 
+    }
+
+    //FINDING ALL TRANSACTION's AND PUTING ON LIST
+    public List collectTransactions (Long id){
+
+        //List<Transaction> transactionList = transactionRepository.findByUserId(id);
+        //return convertTractionListAsDto(transactionList);
+        return null;
+    }
+
+    //Converting Entity to Dto as a List
+    public List<TransactionDTO> convertTractionListAsDto(List<Transaction> transactionList) {
+        List<TransactionDTO> transactionDtoList = new ArrayList<>();
+
+        for (int i = 0; i < transactionList.size(); i++) {
+            TransactionDTO transactionDto= modelMapperConfig.modelMapperForResponse().map(transactionList.get(i), TransactionDTO.class);
+            transactionDtoList.add(transactionDto);
+        }
+
+        return transactionDtoList;
     }
 
 
