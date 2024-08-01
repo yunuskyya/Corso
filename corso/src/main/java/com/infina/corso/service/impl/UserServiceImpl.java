96650355cf04ef90ccd482c4d75870f72531876a
yunuskyya <0yunuskaya0@gmail.com>
2024-08-01 21:22:33 +0300

    UserServiceImpl güncellendi: getAllUser, registerBroker ve registerManager metodları eklendi, registerUser metodu kaldırıldı

--- corso/src/main/java/com/infina/corso/service/impl/UserServiceImpl.java
+++ corso/src/main/java/com/infina/corso/service/impl/UserServiceImpl.java
@@ -2,11 +2,11 @@ package com.infina.corso.service.impl;
 
 import com.infina.corso.config.ModelMapperConfig;
 import com.infina.corso.dto.request.RegisterUserRequest;
+import com.infina.corso.dto.response.GetAllUserResponse;
 import com.infina.corso.model.enums.Role;
 import com.infina.corso.model.User;
 import com.infina.corso.repository.UserRepository;
 import com.infina.corso.service.UserService;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -23,10 +26,15 @@ public class UserServiceImpl implements UserService {
     private PasswordEncoder passwordEncoder;
     private static final Logger logger = LogManager.getLogger(UserServiceImpl.class);
 
-
     @Override
-    @Transactional
-    public void registerUser(RegisterUserRequest registerUserRequest) {
+    public List<GetAllUserResponse> getAllUser() {
+        List<User> brokers = userRepository.findByAuthoritiesContaining(Role.ROLE_BROKER);
+        return brokers.stream()
+                .map(user -> mapper.modelMapperForResponse().map(user, GetAllUserResponse.class))
+                .collect(Collectors.toList());
+    }
+    @Override
+    public void registerBroker(RegisterUserRequest registerUserRequest) {
         User newUser = mapper.modelMapperForRequest().map(registerUserRequest, User.class);
         newUser.setPassword(passwordEncoder.encode(registerUserRequest.getPassword()));
         newUser.setAuthorities(new HashSet<>() {{
@@ -34,9 +42,17 @@ public class UserServiceImpl implements UserService {
         }});
         newUser.setActive(true);
         userRepository.save(newUser);
-        logger.info("User registered: {}", newUser.getUsername());
+        logger.info("Broker registered: {}", newUser.getUsername());
     }
-
-
-
+    @Override
+    public void registerManager(RegisterUserRequest registerUserRequest) {
+        User newUser = mapper.modelMapperForRequest().map(registerUserRequest, User.class);
+        newUser.setPassword(passwordEncoder.encode(registerUserRequest.getPassword()));
+        newUser.setAuthorities(new HashSet<>() {{
+            add(Role.ROLE_MANAGER);
+        }});
+        newUser.setActive(true);
+        userRepository.save(newUser);
+        logger.info("Manager registered: {}", newUser.getUsername());
+    }
 }
