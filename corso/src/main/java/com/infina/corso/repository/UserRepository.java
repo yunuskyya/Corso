9c0ed7de75feccc1fd4c7dc7116bd6737938e9b1
yunuskyya <0yunuskaya0@gmail.com>
2024-08-01 21:14:28 +0300

    UserRepository'e findByAuthoritiesContaining(Role role) metodu eklendi

--- corso/src/main/java/com/infina/corso/repository/UserRepository.java
+++ corso/src/main/java/com/infina/corso/repository/UserRepository.java
@@ -1,15 +1,16 @@
 package com.infina.corso.repository;
 
 import com.infina.corso.model.User;
+import com.infina.corso.model.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
-
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-
+    List<User> findByAuthoritiesContaining(Role role);
 }
