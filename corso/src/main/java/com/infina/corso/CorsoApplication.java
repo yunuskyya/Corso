dacf11a6db7fc4aec7ffc8d045b6ee51c95301a7
Erkal <erkal@example.com>
2024-08-02 23:37:09 +0300

    Kalan değişiklikler

--- corso/src/main/java/com/infina/corso/CorsoApplication.java
+++ corso/src/main/java/com/infina/corso/CorsoApplication.java
@@ -5,13 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.infina.corso.repository")
 @EntityScan(basePackages = "com.infina.corso.model")
 public class CorsoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CorsoApplication.class, args);
-	}
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+        SpringApplication.run(CorsoApplication.class, args);
+
+
+    }
 
 }
