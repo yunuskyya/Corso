4c52bdd4503492e3a7b8d2bc6b194bcc0459b91a
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-03 00:51:09 +0300

    düzenlendi: App.jsx içine ana route'lar eklendi

--- frontend/corso-app/src/App.jsx
+++ frontend/corso-app/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, useNavigate, Routes } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from './redux/hooks'; // Import the custom hook
+import { fetchCurrentUser, logout } from './features/authSlice';
+import Navbar from './components/Common/Navbar.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
+import HomePage from './pages/HomePage.jsx';
+import Login from './components/Auth/Login.jsx';
+import DashboardPage from './pages/DashboardPage.jsx';
+import { Modal } from 'react-bootstrap';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+};
+
+const AppContent = () => {
+  const dispatch = useAppDispatch();
+  const isLoginSuccess = useAppSelector((state) => state.auth.isLoginSuccess);
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if(isLoginSuccess) {
+      dispatch(fetchCurrentUser('admin')).catch(() => {
+        setShowModal(true);
+        setTimeout(() => {
+          dispatch(logout());
+          setShowModal(false);
+          navigate('/login'); // Redirect to login page
+        }, 3000);
+      });
+    }
+  }, [dispatch, navigate, isLoginSuccess]);
+
+  const handleLogout = async () => {
+    dispatch(logout());
+    navigate('/login'); // Redirect to login page
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar onLogout={handleLogout} />
+      <div className="container mt-4">
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="dashboard" element={
+            <PrivateRoute>
+              <DashboardPage />
+            </PrivateRoute>
+          } />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Session Expired</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Your session has expired. You will be logged out.</Modal.Body>
+      </Modal>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
