98b8fb7136e437cfb7f7e9111608027b70c1222f
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-03 00:55:57 +0300

    oluşturuldu: authSlice

---
+++ frontend/corso-app/src/features/authSlice.js
@@ -0,0 +1,64 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { login, fetchUser, logoutUser } from '../api/authApi';
+
+const initialState = {
+    user: null,
+    status: 'idle',
+    error: null,
+    isLoginSuccess: false,
+};
+
+export const loginUser = createAsyncThunk('auth/loginUser', async (credentials) => {
+    const response = await login(credentials);
+    return response;
+});
+
+// delete role here. it is for mock request
+export const fetchCurrentUser = createAsyncThunk('auth/fetchCurrentUser', async (role) => {
+    const response = await fetchUser(role);
+    return response;
+});
+
+export const logout = createAsyncThunk('auth/logout', async () => {
+    await logoutUser();
+});
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(loginUser.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.user = action.payload.user;
+                state.isLoginSuccess = true;
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+                state.isLoginSuccess = false;
+            })
+            .addCase(fetchCurrentUser.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchCurrentUser.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.user = action.payload;
+            })
+            .addCase(fetchCurrentUser.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
+            .addCase(logout.fulfilled, (state) => {
+                state.user = null;
+                state.status = 'idle';
+                state.isLoginSuccess = false;
+            });
+    },
+});
+
+export default authSlice.reducer;
