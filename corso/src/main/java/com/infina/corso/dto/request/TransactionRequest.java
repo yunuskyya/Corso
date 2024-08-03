f2cf3f88971b61693e9a63720b2aa859dfd60674
Erkal <erkal@example.com>
2024-08-03 17:49:24 +0300

    TransactionDTO transaction Request Response olarak sınıflara ayrıldı

---
+++ corso/src/main/java/com/infina/corso/dto/request/TransactionRequest.java
@@ -0,0 +1,14 @@
+package com.infina.corso.dto.request;
+
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+public class TransactionRequest {
+
+    private String accountNumber;
+    private String purchasedCurrency; //Eğer alınan döviz TL ise bu işlemin türü "S(satış)" eğer alınan döviz TL değil ise "A(alış)" olarak kaydedilecek
+    private String soldCurrency;
+    private double amount;
+    //private Long userId;
+}
