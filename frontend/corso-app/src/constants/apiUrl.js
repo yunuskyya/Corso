fd5bb8e74fd26a32b2dd5d4b616802478f4a24fb
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-03 14:15:32 +0300

    eklendi: user url'leri

--- frontend/corso-app/src/constants/apiUrl.js
+++ frontend/corso-app/src/constants/apiUrl.js
@@ -1,6 +1,12 @@
 // base url:
 export const BASE_URL = 'http://localhost:8080/api/v1';
 
-// paths:
+// auth urls:
 export const AUTH_URL = BASE_URL + '/auth';
-export const LOGOUT_URL = BASE_URL + '/logout';
+export const LOGOUT_URL = AUTH_URL;
+
+// user urls:
+export const USER_URL = BASE_URL + '/user';
+export const CURRENT_USER_URL = USER_URL + '/role';
+
+// custo
