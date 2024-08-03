8d3d8a60bb74e2940f55aeb2187338b67aff4e5b
Erkal <erkal@example.com>
2024-08-04 01:25:08 +0300

    Güncelleme

--- corso/src/main/java/com/infina/corso/controller/TransactionController.java
+++ corso/src/main/java/com/infina/corso/controller/TransactionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("api/v1/transaction")
 public class TransactionController {
 
     public TransactionController(TransactionServiceImp transactionServiceImp) {
