1da88a3b2d29155bb290db4f903cc74cb3533f1c
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-03 14:18:59 +0300

    eklendi: debug print

--- frontend/corso-app/src/pages/DashboardPage.jsx
+++ frontend/corso-app/src/pages/DashboardPage.jsx
@@ -5,6 +5,9 @@ import UserDashboard from '../components/Dashboard/UserDashboard.jsx';
 const DashboardPage = () => {
     const { user, isAdmin } = useAuth();
 
+    console.log("isAdmin: ", isAdmin);
+    console.log("user: ", user);
+
     if (isAdmin) {
         return <AdminDashboard />;
     }
