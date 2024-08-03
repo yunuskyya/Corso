2d5850af7fee0d892218b135fa1fba3998d0f210
yunuskyya <0yunuskaya0@gmail.com>
2024-08-03 23:50:42 +0300

    ErrorHandler: oluşturuldu.

---
+++ corso/src/main/java/com/infina/corso/exception/ErrorHandler.java
@@ -0,0 +1,11 @@
+package com.infina.corso.exception;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+public class ErrorHandler {
+  /*ExceptionHandler(MethodArgumentNotValidException.class)
+    public void handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        // do something
+    }*/
+}
