45938a6333a4fcadef4b525bd3b0be4047f9eadb
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-03 14:15:13 +0300

    güncellendi: logout'ta axios instance kullanıldı, currentUser'da parametre kaldırıldı

--- frontend/corso-app/src/api/authApi.js
+++ frontend/corso-app/src/api/authApi.js
@@ -1,5 +1,5 @@
 import {axiosInstance} from './interceptor'
-import { AUTH_URL, LOGOUT_URL } from '../constants/apiUrl'
+import { AUTH_URL, CURRENT_USER_URL, LOGOUT_URL } from '../constants/apiUrl'
 
 export const login = async (credentials) => {
     
@@ -15,10 +15,10 @@ export const login = async (credentials) => {
 
 }
 
-export const fetchUser = async (role) => {
+export const fetchUser = async () => { // role kullanılmıyor
     // const response = await axiosInstance.get(`${AUTH_URL}/currentUser?${role}`);
     // return response.data;
-    return axiosInstance.get(`${AUTH_URL}/currentUser?${role}`)
+    return axiosInstance.get(CURRENT_USER_URL)
     .then((response) => {
         return response.data;
     })
@@ -29,6 +29,6 @@ export const fetchUser = async (role) => {
 }
 
 export const logoutUser = async () => {
-    const response = await axios.delete(LOGOUT_URL);
+    const response = await axiosInstance.delete(LOGOUT_URL);
     return response.data;
 };
