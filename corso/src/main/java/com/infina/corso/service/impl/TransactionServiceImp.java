27ccbfc7d0c397cbe8894264ae9069bffa99c4a5
Erkal <erkal@example.com>
2024-08-04 17:34:26 +0300

    transactionSave methodu test edilip düzeltmeler yapıldı

--- corso/src/main/java/com/infina/corso/service/impl/TransactionServiceImp.java
+++ corso/src/main/java/com/infina/corso/service/impl/TransactionServiceImp.java
@@ -3,10 +3,7 @@ package com.infina.corso.service.impl;
 import com.infina.corso.config.ModelMapperConfig;
 import com.infina.corso.dto.request.TransactionRequest;
 import com.infina.corso.dto.response.TransactionResponse;
-import com.infina.corso.model.Account;
-import com.infina.corso.model.Currency;
-import com.infina.corso.model.Transaction;
-import com.infina.corso.model.User;
+import com.infina.corso.model.*;
 import com.infina.corso.repository.AccountRepository;
 import com.infina.corso.repository.CurrencyRepository;
 import com.infina.corso.repository.TransactionRepository;
@@ -14,6 +11,7 @@ import com.infina.corso.repository.UserRepository;
 import com.infina.corso.service.TransactionService;
 import com.infina.corso.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -50,39 +48,40 @@ public class TransactionServiceImp implements TransactionService {
     private final AccountRepository accountRepository; //AccountService sınıfı oluşturulduğunda oradan çekilecek
 
 
-
-
+    @Transactional
     public void transactionSave(TransactionRequest transactionRequest) {
         Transaction transaction = modelMapperConfig.modelMapperForRequest().map(transactionRequest, Transaction.class);
         //Transaction'un türü belirlenip set edilir.
-        if(transaction.getSoldCurrency().equals("TL")){
+        if (transaction.getSoldCurrency().equals("TL")) {
             transaction.setTransactionType('A');
-        }else transaction.setTransactionType('S');
+        } else transaction.setTransactionType('S');
         //Gelen request içindeki account no ile ilgli Account sınıfı bulunur
         Account account = accountRepository.findByAccountNumber(transactionRequest.getAccountNumber());
         //Yapılan transaction ilgili account sınıfı içindeki Transaction listesine eklenir
         account.getTransactions().add(transaction);
         //İlgili account içindeki bakiye değişikliği yapılır
         BigDecimal balance = account.getBalance();
-        BigDecimal cost =  calculateTransactionCost(transaction.getTransactionType(), account.getAccountNumber(), transaction.getAmount(), transaction.getPurchasedCurrency());
+        BigDecimal cost = calculateTransactionCost(transaction.getTransactionType(), transaction.getAmount(), transaction.getPurchasedCurrency());
         BigDecimal newBalance = balance.subtract(cost);
         account.setBalance(newBalance);
         //account bakiyesi güncellendikten sonra veritabanına kaydedilir
         accountRepository.save(account);
+        transaction.setAccount(account);
         //Transaction veritabanına kaydedilir
+        System.out.println(transaction.toString());
         transactionRepository.save(transaction);
 
     }
 
+
     //Yapılan transaction işleminin maliyet hesabı
-    public BigDecimal calculateTransactionCost(char transactionType, String accountNumber, double amount, String currencyCode) {
-        Account account = accountRepository.findByAccountNumber(accountNumber);
+    public BigDecimal calculateTransactionCost(char transactionType, double amount, String currencyCode) {
         Currency currency = currencyRepository.findByCode(currencyCode);
         double currencyPrice;
-        if(transactionType == 'S'){
+        if (transactionType == 'S') {
             currencyPrice = Double.parseDouble(currency.getBuying());
-        }else currencyPrice = Double.parseDouble(currency.getSelling());
-        BigDecimal cost = new BigDecimal(currencyPrice*amount);
+        } else currencyPrice = Double.parseDouble(currency.getSelling());
+        BigDecimal cost = new BigDecimal(currencyPrice * amount);
         return cost;
     }
 
@@ -95,10 +94,33 @@ public class TransactionServiceImp implements TransactionService {
         return null;
     }
 
+
+    /* //Gelen id ile ilgili kullanıcı veritabanından bulunur --> bulunnan kullanıcının Customer listesi alınır--
+        //--> Customer listesinin içindeki Account listeleri alınır --> Account listeleri içindeki Transaction Listeleri bir listeye toplanır
+        Optional<User> user = userRepository.findById(id);
+        List<Customer> customerList = user.get().getCustomerList();
+        List<Account> accountList = new ArrayList<>();
+        customerList.forEach(customer -> customer.getAccounts().addAll(accountList));
+        List<Transaction> transactionList = new ArrayList<>();
+        accountList.forEach(account -> account.getTransactions().addAll(transactionList));
+        return convertTractionListAsDto(transactionList); */
+
+
     //Adminin veya Yönetici kullanıcısının brokerların yaptığı tüm işlemleri getiren method
     public List<TransactionResponse> collectAllTransactions(){
         List<Transaction> transactionList = transactionRepository.findAll();
         return convertTractionListAsDto(transactionList);
+    }
+
+    //Entity listesinin Dto listesine çevrimi ** User ID'ye göre **
+    public List<TransactionResponse> convertTractionListAsDto(List<Transaction> transactionList, int userId) {
+        return transactionList.stream()
+                .map(transaction -> {
+                    TransactionResponse response = modelMapperConfig.modelMapperForResponse().map(transaction, TransactionResponse.class);
+                    response.setUserId((int) userId);  // userId'yi DTO'ya set ediyoruz
+                    return response;
+                })
+                .collect(Collectors.toList());
     }
 
     //Entity listesinin Dto listesine çevrimi
@@ -108,9 +130,6 @@ public class TransactionServiceImp implements TransactionService {
                 .map(transaction, TransactionResponse.class))
                 .collect(Collectors.toList());
     }
-
-
-
 
 
 }
