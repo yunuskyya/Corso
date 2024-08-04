1ecc3c7171f25d2f1463f7a2c43535f64f0e3273
yunuskyya <0yunuskaya0@gmail.com>
2024-08-04 15:34:48 +0300

    AccountController: güncellendi.

--- corso/src/main/java/com/infina/corso/controller/AccountController.java
+++ corso/src/main/java/com/infina/corso/controller/AccountController.java
@@ -2,47 +2,53 @@ package com.infina.corso.controller;
 
 import com.infina.corso.model.Account;
 import com.infina.corso.service.AccountService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/accounts")
+@Tag(name = "Account Management", description = "Operations related to account management")
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
 
-    @PostMapping("/create")
-    public Account createAccount(@RequestBody Account account) {
-        return accountService.createAccount(account);
-    }
-    /*
-    @PutMapping("/update/{id}")
-    public Account updateAccount(@PathVariable Long id, @RequestBody Account account) {
-        return accountService.updateAccount(id, account);
+    @GetMapping
+    public ResponseEntity<List<Account>> getAllAccounts() {
+        return ResponseEntity.ok(accountService.getAllAccounts());
     }
 
-     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
 
-    @DeleteMapping("/delete/{id}")
-    public void deleteAccount(@PathVariable Long id) {
+    @GetMapping("/customer/{customerId}")
+    public ResponseEntity<List<Account>> getAccountsByCustomerId(@PathVariable Long customerId) {
+        return ResponseEntity.ok(accountService.getAccountsByCustomerId(customerId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(accountService.createAccount(account));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Account> updateAccount(@PathVariable Long id, @RequestBody Account account) {
+        return ResponseEntity.ok(accountService.updateAccount(id, account));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
         accountService.deleteAccount(id);
+        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/get/{id}")
-    public Account getAccountById(@PathVariable Long id) {
-        return accountService.getAccountById(id);
-    }
 
-    @GetMapping("/get/all")
-    public List<Account> getAllAccounts() {
-        return accountService.getAllAccounts();
-    }
-
-    @GetMapping("/get/accountNumber/{accountNumber}")
-    public Account getAccountByAccountNumber(@PathVariable String accountNumber) {
-        return accountService.getAccountByAccountNumber(accountNumber);
-    }
 }
