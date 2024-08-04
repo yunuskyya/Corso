2f219c04613d9396f4cdea40c106a6b40c69fa75
Erkal <erkal@example.com>
2024-08-04 17:38:36 +0300

    TransactionResponse sınıfına Getter Setter eklendi

--- corso/src/main/java/com/infina/corso/dto/response/TransactionResponse.java
+++ corso/src/main/java/com/infina/corso/dto/response/TransactionResponse.java
@@ -1,10 +1,15 @@
 package com.infina.corso.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class TransactionResponse {
 
-    private Long userId;
+    private int userId;
     private char transactionType;
     private String purchasedCurrency;
     private String soldCurrency;
