42efc579141881fe4cb62b1fec39edbc0e16a58e
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-05 09:20:48 +0300

    eklendi: CustomerController

--- corso/src/main/java/com/infina/corso/controller/CustomerController.java
+++ corso/src/main/java/com/infina/corso/controller/CustomerController.java
@@ -1,2 +1,23 @@
-package com.infina.corso.controller;public class CustomerController {
+package com.infina.corso.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user/{userId}/customer/{customerId}/account")
+public class CustomerController {
+
+    @GetMapping
+    public String getCustomer(@PathVariable("userId") Long userId) {
+        return "ID'si " + userId + " olan broker'ın tüm Customers";
+    }
+
+    @GetMapping("/{id}") // /api/v1/user/customer/1
+    public String getCustomerById(@PathVariable Long id, @PathVariable("userId") Long userId){
+        return "ID'si " + userId + " olan broker'ın ID'si " + id + " olan Customer";
+    }
+
+    // /api/v1/customer?brokerId=1
 }
