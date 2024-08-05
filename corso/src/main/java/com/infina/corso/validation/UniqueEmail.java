e8d5183361f37bf5da0487a4ababe64d751321c6
yunuskyya <0yunuskaya0@gmail.com>
2024-08-05 13:26:38 +0300

    UniqueEmail annotation sınıfı güncellendi

--- corso/src/main/java/com/infina/corso/validation/UniqueEmail.java
+++ corso/src/main/java/com/infina/corso/validation/UniqueEmail.java
@@ -1,4 +1,16 @@
 package com.infina.corso.validation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface UniqueEmail {
+    String message() default "Email already exists";
+
+    Class<?>[] groups() default {};
+
+    Class<?>[] payload() default {};
 }
