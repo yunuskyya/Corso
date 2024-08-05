5c5a03778a9856b0494db813a3047ddc2ddcc5f0
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-05 09:48:05 +0300

    güncellendi: modele göre alanlar düzenlendi ve validasyon eklendi

--- corso/src/main/java/com/infina/corso/dto/request/CustomerRequest.java
+++ corso/src/main/java/com/infina/corso/dto/request/CustomerRequest.java
@@ -1,5 +1,8 @@
 package com.infina.corso.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +13,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CustomerRequest {
+    @NotNull
     private String name;
+    @NotNull
     private String surname;
     private String tcKimlikNo;
-    private String companyNo;
+    private String companyName;
+    private String vkn;
+    @Pattern(regexp = "^(05)[0-9]{9}$") // 05XX XXX XX XX // arada boşluk olmadan
+    private String phone;
+    @Email
     private String email;
     private Long userId;
 }
