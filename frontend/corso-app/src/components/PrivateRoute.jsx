6b90584dadee2a07bc27048716198780fea821f7
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-05 09:16:16 +0300

    eklendi: user için istek atılırken spinner ekranda döner

--- frontend/corso-app/src/components/PrivateRoute.jsx
+++ frontend/corso-app/src/components/PrivateRoute.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAppSelector } from '../redux/hooks';  
+import useAuth from '../hooks/useAuth';
+import { GeneralSpinner } from './Common/GeneralSpinner';
+import { Unauthorized } from './Auth/Unauthorized';
 
 const PrivateRoute = ({ children }) => {
-    const user = useAppSelector((state) => state.auth.user);
+    const { user, fetch_status, status } = useAuth();
 
-    console.log(user);
+    console.log("fetchStatus ", fetch_status);
 
-    return user ? children : <Navigate to="/login" />;
+    return user ? children : fetch_status === 'loading'? <GeneralSpinner /> : <Navigate to="/login" /> ;
+    // fetchStatus === 'loading'? <GeneralSpinner /> : <Unauthorized />;
+
+    // return user ? children : fetchStatus==='failed'? <Navigate to="/login" /> : 
+    // fetchStatus === 'loading'? <GeneralSpinner /> : <Unauthorized />;
 };
 
 export default PrivateRoute;
