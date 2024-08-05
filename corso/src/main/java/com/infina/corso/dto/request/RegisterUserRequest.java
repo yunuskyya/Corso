40f5357de67d28b09645f6d951abe2c6d88c42ca
yunuskyya <0yunuskaya0@gmail.com>
2024-08-05 13:02:25 +0300

    Güncellendi: RegisterUserRequest doğrulama mesajları eklendi

--- corso/src/main/java/com/infina/corso/dto/request/RegisterUserRequest.java
+++ corso/src/main/java/com/infina/corso/dto/request/RegisterUserRequest.java
@@ -1,26 +1,35 @@
 package com.infina.corso.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterUserRequest {
     @Size(min = 3, max = 50)
-    @NotBlank
+    @NotBlank(message = "{corso.validation.notBlank.firstName}")
     private String firstName;
+
     @Size(min = 3, max = 50)
-    @NotBlank
+    @NotBlank(message = "{corso.validation.notBlank.lastName}")
     private String lastName;
+
     @Size(min = 3, max = 50)
-    @NotBlank
+    @NotBlank(message = "{corso.validation.notBlank.username}")
     private String username;
+
     @Size(min = 3, max = 50)
-    @NotBlank
+    @NotBlank(message = "{corso.validation.notBlank.email}")
+    @Email(message = "{corso.validation.email}")
     private String email;
-    @Size(min = 3, max = 50)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\.]).{8,}$",message = "corso.validation.pattern.password")
+
+    @Size(min = 5, max = 50, message = "{corso.validation.size.password}")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\.]).{8,}$", message = "{corso.validation.pattern.password}")
     private String password;
 }
