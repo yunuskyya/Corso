1c9c2ad80c539aa0804479fd15f8212852e3a4fe
yunuskyya <0yunuskaya0@gmail.com>
2024-08-07 00:01:52 +0300

    GetAllAccountResponse: güncellendi

--- corso/src/main/java/com/infina/corso/dto/response/GetAllAccountResponse.java
+++ corso/src/main/java/com/infina/corso/dto/response/GetAllAccountResponse.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
-public class AccountResponse {
+public class GetAllAccountResponse {
     private String accountNumber;
     private String iban;
     private String bankName;
