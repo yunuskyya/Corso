b2cf63812d407110555909c2ff0db67715ac4cf0
yunuskyya <0yunuskaya0@gmail.com>
2024-08-07 02:03:45 +0300

    UpdateAccountRequest: Güncellendi.

--- corso/src/main/java/com/infina/corso/dto/request/UpdateAccountRequest.java
+++ corso/src/main/java/com/infina/corso/dto/request/UpdateAccountRequest.java
@@ -2,10 +2,12 @@ package com.infina.corso.dto.request;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class UpdateAccountRequest {
     private String accountNumber;
     private String bankName;
     private String currency;
-    private String balance;
+    private BigDecimal balance;
 }
