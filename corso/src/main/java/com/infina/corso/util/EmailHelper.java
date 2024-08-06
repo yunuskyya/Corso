9dcc7f66a5a50f9342e905d8478cd7cfda1066cb
yunuskyya <0yunuskaya0@gmail.com>
2024-08-06 22:03:50 +0300

    EmailHelper: sendRegistrationEmail ve  sendActivationEmail mototları oluşturuldu.

--- corso/src/main/java/com/infina/corso/util/EmailHelper.java
+++ corso/src/main/java/com/infina/corso/util/EmailHelper.java
@@ -13,4 +13,35 @@ public class EmailHelper {
         this.mailService = mailService;
     }
 
+    public void sendRegistrationEmail(User user, String rawPassword) {
+        String subject = "Kayıt Onayı";
+        String text = String.format(
+                "Merhaba %s %s,\n\n" +
+                        "Sistemimize başarıyla kayıt oldunuz.\n\n" +
+                        "Kullanıcı Adınız: %s\n" +
+                        "Geçici Şifreniz: %s\n\n" +
+                        "Güvenliğiniz için lütfen şifrenizi değiştirmek için sisteme giriş yapın veya aşağıdaki bağlantıyı kullanın:\n" +
+                        "http://localhost:8080/swagger-ui/index.html#/User%%20Management/changePassword\n\n" +
+                        "Teşekkürler,\n" +
+                        "Infina Corso Ekibi",
+                user.getFirstName(),
+                user.getLastName(),
+                user.getUsername(),
+                rawPassword
+        );
+        mailService.sendSimpleMessage(user.getEmail(), subject, text);
+    }
+
+    public void sendActivationEmail(User user) {
+        String subject = "Hesap Aktifleştirildi";
+        String text = String.format(
+                "Merhaba %s %s,\n\n" +
+                        "Hesabınız başarıyla aktifleştirildi.\n\n" +
+                        "Teşekkürler,\n" +
+                        "Infina Corso Ekibi",
+                user.getFirstName(),
+                user.getLastName()
+        );
+        mailService.sendSimpleMessage(user.getEmail(), subject, text);
+    }
 }
