cdb087536155a134d504d8765da4d58e602cbfe9
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-08 10:26:30 +0300

    eklendi: filter metodu eklendi

--- corso/src/main/java/com/infina/corso/service/CustomerService.java
+++ corso/src/main/java/com/infina/corso/service/CustomerService.java
@@ -1,7 +1,9 @@
 package com.infina.corso.service;
 
+import com.infina.corso.dto.request.CustomerFilterRequest;
 import com.infina.corso.dto.request.CustomerUpdateRequest;
 import com.infina.corso.dto.response.CustomerByBrokerResponse;
+import com.infina.corso.dto.response.CustomerFilterResponse;
 import com.infina.corso.dto.response.CustomerGetByIdResponse;
 import com.infina.corso.dto.response.CustomerResponse;
 import org.springframework.data.domain.Page;
@@ -15,5 +17,5 @@ public interface CustomerService {
     void createCustomer(CustomerUpdateRequest customerDto);
     CustomerResponse updateCustomer(Long id, CustomerUpdateRequest customerDto);
     void deleteCustomer(Long id);
-
+    Page<CustomerFilterResponse> filterCustomers(CustomerFilterRequest filterRequest, Pageable pageable);
 }
