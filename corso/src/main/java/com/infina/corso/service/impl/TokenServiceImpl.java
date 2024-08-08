fddaa29351129a44cd928b162b8b2a410a25a7af
yunuskyya <0yunuskaya0@gmail.com>
2024-08-08 10:54:36 +0300

    TokenService ve TokenServiceImpl :generateToken metodu user dönücek şekilde  güncellendi

--- corso/src/main/java/com/infina/corso/service/impl/TokenServiceImpl.java
+++ corso/src/main/java/com/infina/corso/service/impl/TokenServiceImpl.java
@@ -27,16 +27,11 @@ public class TokenServiceImpl implements TokenService {
     private final ObjectMapper objectmapper;
 
     @Override
-    public Token generateToken(GetUserByEmailResponse user, CredentialsRequest credentials) {
-        User inDb = userRepository.findById(user.getId()).orElseThrow(() -> {
-            logger.error("User not found: " + user.getId());
-            throw new IllegalArgumentException("User with ID " + user.getId() + " not found");
-
-        });
+    public Token generateToken(User user) {
         Token token = new Token();
         token.setTokenId(UUID.randomUUID().toString());
-        token.setUser(inDb);
-        token.setExpirationDate(System.currentTimeMillis() + (5 * 60 * 1000));
+        token.setUser(user);
+        token.setExpirationDate(System.currentTimeMillis() + (3 * 60 * 60 * 1000));
         redisTemplate.opsForValue().set(token.getTokenId(), token);
         logger.info("Token generated: {}", token.getTokenId());
         return token;
