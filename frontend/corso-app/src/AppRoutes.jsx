257a9ab8b530b13b1819510f22897ede10d3ce19
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-09 12:08:47 +0300

    güncellendi: yeni route'lar eklendi

--- frontend/corso-app/src/AppRoutes.jsx
+++ frontend/corso-app/src/AppRoutes.jsx
@@ -3,6 +3,8 @@ import HomePage from './pages/HomePage';
 import Login from './components/Auth/Login';
 import DashboardPage from './pages/DashboardPage';
 import PrivateRoute from './components/PrivateRoute';
+import AddCustomerPage from './pages/AddCustomerPage';
+import AccountsPage from './pages/AccountsPage';
 
 
 const AppRoutes = () => {
@@ -10,6 +12,8 @@ const AppRoutes = () => {
         <Routes>
             <Route exact path="/" element={<HomePage />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/addCustomer" element={<AddCustomerPage />} />
+            <Route path="/accounts" element={<AccountsPage />} />
             <Route path="/dashboard" element={
                 <PrivateRoute>
                     <DashboardPage />
