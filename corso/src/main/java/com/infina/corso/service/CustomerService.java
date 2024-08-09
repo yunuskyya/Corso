426883cc946961326b55fb241981a242a5d1c8ea
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-09 09:26:50 +0300

    ekle: customer filter hepsini getir, paged getir

--- corso/src/main/java/com/infina/corso/service/CustomerService.java
+++ corso/src/main/java/com/infina/corso/service/CustomerService.java
@@ -9,6 +9,8 @@ import com.infina.corso.dto.response.CustomerResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 
 public interface CustomerService {
     CustomerGetByIdResponse getCustomerById(Long id);
@@ -17,5 +19,6 @@ public interface CustomerService {
     void createCustomer(CustomerUpdateRequest customerDto);
     CustomerResponse updateCustomer(Long id, CustomerUpdateRequest customerDto);
     void deleteCustomer(Long id);
-    Page<CustomerFilterResponse> filterCustomers(CustomerFilterRequest filterRequest, Pageable pageable);
+    Page<CustomerFilterResponse> filterCustomersPaged(CustomerFilterRequest filterRequest, Pageable pageable);
+    List<CustomerFilterResponse> filterCustomers(CustomerFilterRequest filterRequest);
 }
