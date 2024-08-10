4b62c04d046b32341be61245ebb64f57fe454275
Erkal <erkal@example.com>
2024-08-10 14:27:33 +0300

    Gün sonu işlemleri için SystemDate sınıfı oluşturulup servisleri yazıldı

---
+++ corso/src/main/java/com/infina/corso/repository/SystemDateRepository.java
@@ -0,0 +1,7 @@
+package com.infina.corso.repository;
+
+import com.infina.corso.model.SystemDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SystemDateRepository extends JpaRepository<SystemDate, Integer> {
+}
