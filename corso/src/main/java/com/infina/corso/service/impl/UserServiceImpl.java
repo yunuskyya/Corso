06a3af315a3c83aa9043e7096ae9a3a977f046c4
yunuskyya <0yunuskaya0@gmail.com>
2024-08-10 19:07:57 +0300

    UserServiceImpl: resetPassword ve updatePassword alanları eklendi.

--- corso/src/main/java/com/infina/corso/service/impl/UserServiceImpl.java
+++ corso/src/main/java/com/infina/corso/service/impl/UserServiceImpl.java
@@ -1,15 +1,16 @@
 package com.infina.corso.service.impl;
 
 import com.infina.corso.config.ModelMapperConfig;
-import com.infina.corso.dto.request.ChangePasswordRequest;
-import com.infina.corso.dto.request.RegisterUserRequest;
+import com.infina.corso.dto.request.*;
 import com.infina.corso.dto.response.GetAllUserResponse;
+import com.infina.corso.exception.AccessDeniedException;
 import com.infina.corso.exception.UserNotFoundException;
+import com.infina.corso.model.Token;
 import com.infina.corso.model.Transaction;
 import com.infina.corso.model.User;
 import com.infina.corso.model.enums.Role;
 import com.infina.corso.repository.UserRepository;
-import com.infina.corso.service.MailService;
+import com.infina.corso.service.TokenService;
 import com.infina.corso.service.UserService;
 import com.infina.corso.util.EmailHelper;
 import jakarta.validation.Valid;
@@ -24,6 +25,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -33,7 +35,7 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private ModelMapperConfig mapper;
     private PasswordEncoder passwordEncoder;
-    private MailService emailService;
+    private TokenService tokenService;
     private static final Logger logger = LogManager.getLogger(UserServiceImpl.class);
 
     @Override
@@ -44,20 +46,21 @@ public class UserServiceImpl implements UserService {
     @Override
     public void registerBroker(@Valid RegisterUserRequest registerUserRequest) {
         User newUser = mapper.modelMapperForRequest().map(registerUserRequest, User.class);
-        String rawPassword = registerUserRequest.getPassword();  // Ham şifreyi saklayın
-        newUser.setPassword(passwordEncoder.encode(rawPassword));
+        newUser.setPassword(passwordEncoder.encode(""));
         newUser.setAuthorities(new HashSet<>() {{
             add(Role.ROLE_BROKER);
         }});
         newUser.setActive(true);
+        newUser.setResetPasswordToken(UUID.randomUUID().toString());// Bu eklendikten sonra login olunca şifre değiştirme ekranı gelecek
         userRepository.save(newUser);
         logger.info("Broker registered: {}", newUser.getUsername());
-        emailHelper.sendRegistrationEmail(newUser, rawPassword);
+        emailHelper.sendTokenEmail(newUser.getEmail(), newUser.getResetPasswordToken());
     }
+
     @Override
-    public void registerManager(@Valid RegisterUserRequest registerUserRequest) {
-        User newUser = mapper.modelMapperForRequest().map(registerUserRequest, User.class);
-        String rawPassword = registerUserRequest.getPassword();  // Ham şifreyi saklayın
+    public void registerManager(@Valid RegisterManagerRequest request) {
+        User newUser = mapper.modelMapperForRequest().map(request, User.class);
+        String rawPassword = request.getPassword();  // Ham şifreyi saklayın
         newUser.setPassword(passwordEncoder.encode(rawPassword));
         newUser.setAuthorities(new HashSet<>() {{
             add(Role.ROLE_MANAGER);
@@ -65,7 +68,7 @@ public class UserServiceImpl implements UserService {
         newUser.setActive(true);
         userRepository.save(newUser);
         logger.info("Manager registered: {}", newUser.getEmail());
-        emailHelper.sendRegistrationEmail(newUser, rawPassword);
+        emailHelper.sendRegistrationEmail(newUser);
 
     }
     @Override
@@ -81,22 +84,6 @@ public class UserServiceImpl implements UserService {
         userRepository.save(user);
     }
 
-    @Override
-    public void activateUserByEmail(String email) {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Kullanıcı bulunamadı"));
-
-        if (!user.isAccountLocked()) {
-            throw new RuntimeException("Hesap zaten aktif.");
-        }
-
-        user.setAccountLocked(false);
-        userRepository.save(user);
-        logger.info("Kullanıcı aktifleştirildi: {}", user.getUsername());
-        emailHelper.sendActivationEmail(user);
-    }
-
-
     public List<Transaction> getAllTransactionsById(int id){
         Optional<User> user = userRepository.findById(id);
         return user.get().getTransactions();
@@ -110,5 +97,35 @@ public class UserServiceImpl implements UserService {
       userInDB.setDeleted(true);
         userRepository.save(userInDB);
         logger.info("User deleted: {}", userInDB.getUsername());
+    }
+
+
+
+
+    @Override
+    public void resetPassword(PasswordResetRequest passwordResetRequest) {
+        User userInDb = userRepository.findByEmail(passwordResetRequest.getEmail()).orElseThrow(() -> {
+            logger.error("User not found with email: {}", passwordResetRequest.getEmail());
+            return new UserNotFoundException("User not found with email: " + passwordResetRequest.getEmail());
+        });
+        if (userInDb.isDeleted() || !userInDb.isActive()) {
+            logger.debug("User is not active or deleted: {}", passwordResetRequest.getEmail());
+            throw new AccessDeniedException();
+        }
+        userInDb.setResetPasswordToken(UUID.randomUUID().toString());
+        userRepository.save(userInDb);
+        emailHelper.sendTokenEmail(passwordResetRequest.getEmail(), userInDb.getResetPasswordToken());
+    }
+
+    @Override
+    public void updatePassword(String token, UpdatePasswordRequest request) {
+        User userInDb = userRepository.findByResetPasswordToken(token).orElseThrow(() -> {
+            logger.error("Invalid password reset token: {}", token);
+            return new RuntimeException("Invalid password reset token: " + token);
+        });
+        userInDb.setPassword(passwordEncoder.encode(request.getPassword()));
+        userInDb.setResetPasswordToken(null);
+        userRepository.save(userInDb);
+        logger.debug("Password updated successfully: {}", userInDb.getEmail());
     }
 }
