c77b5b37a9921d54f2e1f6226710fe56af490745
Erkal <erkal@example.com>
2024-08-10 15:27:55 +0300

    Iban save methodu dolduruldu

--- corso/src/main/java/com/infina/corso/service/impl/IbanServiceImpl.java
+++ corso/src/main/java/com/infina/corso/service/impl/IbanServiceImpl.java
@@ -1,9 +1,15 @@
 package com.infina.corso.service.impl;
 
+import com.infina.corso.dto.request.IbanRegisterRequest;
+import com.infina.corso.model.Customer;
 import com.infina.corso.model.Iban;
+import com.infina.corso.repository.CustomerRepository;
 import com.infina.corso.repository.IbanRepository;
+import com.infina.corso.service.CustomerService;
 import com.infina.corso.service.IbanService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,14 +19,22 @@ import java.util.Optional;
 public class IbanServiceImpl implements IbanService {
 
     private final IbanRepository ibanRepository;
+    private final CustomerRepository customerRepository;
+    private final ModelMapper modelMapperForRequest;
 
     @Autowired
-    public IbanServiceImpl(IbanRepository ibanRepository) {
+    public IbanServiceImpl(IbanRepository ibanRepository, CustomerRepository customerRepository, @Qualifier("modelMapperForRequest") ModelMapper modelMapperForRequest) {
         this.ibanRepository = ibanRepository;
+        this.customerRepository = customerRepository;
+        this.modelMapperForRequest = modelMapperForRequest;
     }
 
     @Override
-    public Iban saveIban(Iban iban) {
+    public Iban saveIban(IbanRegisterRequest ibanRegisterRequest, Long customerId) {
+        Optional<Customer> customer = customerRepository.findById(customerId);
+        Iban iban = modelMapperForRequest.map(ibanRegisterRequest, Iban.class);
+        customer.get().getIbans().add(iban);
+        customerRepository.save(customer.get());
         return ibanRepository.save(iban);
     }
 
