33afd8b8e99109b984d2b6316ad01d96166eddb4
yunuskyya <0yunuskaya0@gmail.com>
2024-08-11 02:19:59 +0300

    RegisterUserRequest: phone field'ı eklendi.

--- corso/src/main/java/com/infina/corso/dto/request/RegisterUserRequest.java
+++ corso/src/main/java/com/infina/corso/dto/request/RegisterUserRequest.java
@@ -3,7 +3,6 @@ package com.infina.corso.dto.request;
 import com.infina.corso.validation.annotations.UniqueEmail;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,9 +20,13 @@ public class RegisterUserRequest {
     @NotBlank(message = "{corso.validation.notBlank.lastName}")
     private String lastName;
 
-    @Size(min = 3, max = 50)
+    @Size(min = 5, max = 50)
     @NotBlank(message = "{corso.validation.notBlank.username}")
     private String username;
+
+    @Size(min = 5, max = 50)
+    @NotBlank
+    private String phone;
 
     @Size(min = 8, max = 50)
     @NotBlank(message = "{corso.validation.notBlank.email}")
