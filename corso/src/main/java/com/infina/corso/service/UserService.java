833fa31c2e418b9bf851f182b2ee75fdcbba9a23
yunuskyya <0yunuskaya0@gmail.com>
2024-08-10 15:46:11 +0300

    UserService: updatePassword ve resetPassword mototları eklendi.

--- corso/src/main/java/com/infina/corso/service/UserService.java
+++ corso/src/main/java/com/infina/corso/service/UserService.java
@@ -1,7 +1,6 @@
 package com.infina.corso.service;
 
-import com.infina.corso.dto.request.ChangePasswordRequest;
-import com.infina.corso.dto.request.RegisterUserRequest;
+import com.infina.corso.dto.request.*;
 import com.infina.corso.dto.response.GetAllUserResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,8 +8,9 @@ import org.springframework.data.domain.Pageable;
 public interface UserService {
     Page<GetAllUserResponse> getAllUser(Pageable pageable);
     void registerBroker(RegisterUserRequest registerUserRequest);
-    void registerManager(RegisterUserRequest registerUserRequest);
+    void registerManager(RegisterManagerRequest request);
     void changePassword(ChangePasswordRequest changePasswordRequest);
-    void activateUserByEmail(String email);
     void deleteUser(int id);
+    void updatePassword(String token, UpdatePasswordRequest request);
+    void resetPassword(PasswordResetRequest passwordResetRequest);
 }
