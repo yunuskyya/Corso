f00f3fd60ee04239f60057f46918e5eddc91c045
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-11 13:48:52 +0300

    güncellendi: dto'ya uygun veri erişimi yapıldı

--- frontend/corso-app/src/hooks/useAuth.js
+++ frontend/corso-app/src/hooks/useAuth.js
@@ -5,13 +5,15 @@ const useAuth = () => {
     const fetch_status = useAppSelector((state) => state.auth.fetch_status);
     const status = useAppSelector((state) => state.auth.status);
 
-    console.log("authority", user? user.authorities[0] : "no user");
+    console.log("user: ", user);
+
+    console.log("authority", user ? user.role : "no user");
     return {
         user,
         status,
         fetch_status,
-        isAuthenticated: user!== null && user !== undefined,
-        isAdmin: user?.authorities[0]['authority'] === 'ROLE_ADMIN',
+        isAuthenticated: user !== null && user !== undefined,
+        role: user?.role,
     };
 };
 
