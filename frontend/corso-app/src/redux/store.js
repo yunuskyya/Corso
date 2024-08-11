378d8790a552b14574948717a0d8200a182d76de
yunuskyya <0yunuskaya0@gmail.com>
2024-08-12 01:19:27 +0300

    stor.js : userSilc tanımlandı.

--- frontend/corso-app/src/redux/store.js
+++ frontend/corso-app/src/redux/store.js
@@ -2,11 +2,14 @@ import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../features/authSlice';
 import customerReducer from '../features/customerSlice';
 import accountReducer from '../features/accountSlice';
+import userReducer from '../features/userSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         customer: customerReducer,
-        account: accountReducer
+        account: accountReducer,
+        user: userReducer
+
     }
 });
