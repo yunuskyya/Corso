592b7fd192534e5932e2f352cddd80fa56f546dd
yunuskyya <0yunuskaya0@gmail.com>
2024-08-12 01:20:14 +0300

    apiUrl.js: user ile ilgili apile tanımlandı.

--- frontend/corso-app/src/constants/apiUrl.js
+++ frontend/corso-app/src/constants/apiUrl.js
@@ -17,3 +17,13 @@ export const CUSTOMER_URL = BASE_URL + 'customer';
 
 export const ACCOUNT_URL = BASE_URL + 'accounts';
 export const ACCOUNT_URL_CUSTOMER = BASE_URL + 'accounts/faketest';
+
+// user urls:
+export const USER_REGISTER_BROKER_URL = USER_URL + '/register/broker';  // Broker kaydı
+export const USER_REGISTER_MANAGER_URL = USER_URL + '/register/manager'; // Manager kaydı
+export const USER_CHANGE_PASSWORD_URL = USER_URL + '/change-password';   // Şifre değişikliği
+export const USER_RESET_PASSWORD_URL = USER_URL + '/reset-password';     // Şifre sıfırlama
+export const USER_ACTIVATE_URL = USER_URL + '/activate';                 // Kullanıcı aktif etme
+export const USER_UNBLOCK_URL = USER_URL + '/unBlock';                   // Kullanıcı blok kaldırma
+export const USER_DELETE_URL = USER_URL + '/{id}';
+export const USER_LIST_URL = USER_URL + '/all';
