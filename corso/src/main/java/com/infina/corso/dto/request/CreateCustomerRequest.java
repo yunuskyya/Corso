385cc78211a34291b98f1c9b85b032a6e6a5b384
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-11 20:47:53 +0300

    eklendi: create customer dto

---
+++ corso/src/main/java/com/infina/corso/dto/request/CreateCustomerRequest.java
@@ -0,0 +1,38 @@
+package com.infina.corso.dto.request;
+
+import com.infina.corso.model.enums.CustomerStatus;
+import com.infina.corso.model.enums.CustomerType;
+import com.infina.corso.validation.annotations.ValidCreateCustomerRequest;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ValidCreateCustomerRequest
+public class CreateCustomerRequest {
+    private String name;
+
+    private String surname;
+
+    @NotNull
+    private CustomerType customerType;
+    @NotNull
+    private CustomerStatus status;
+
+    private String tcKimlikNo;
+    private String companyName;
+    private String vkn;
+
+    @Pattern(regexp = "^(05)[0-9]{9}$") @NotNull // 05XX XXX XX XX // arada boşluk olmadan
+    private String phone;
+
+    @NotNull @Email
+    private String email;
+}
