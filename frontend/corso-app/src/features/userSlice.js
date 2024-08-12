430d14b095665811eeda3a38f3ff75012d3de06a
yunuskyya <0yunuskaya0@gmail.com>
2024-08-13 02:05:20 +0300

    userSlice.js: güncellendi.

--- frontend/corso-app/src/features/userSlice.js
+++ frontend/corso-app/src/features/userSlice.js
@@ -78,6 +78,7 @@ export const deleteUser = createAsyncThunk(
             const response = await apiDeleteUser(deleteUserRequest);
             return response;
         } catch (error) {
+            console.log('ASDJJKASJKDK');
             return rejectWithValue(error.response.data);
         }
     }
