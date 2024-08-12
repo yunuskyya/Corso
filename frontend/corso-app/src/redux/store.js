85dd3fd1fb3fc21b3b9c40f5fa8e423b62508364
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-12 16:52:18 +0300

    eklendi: customerListReducer

--- frontend/corso-app/src/redux/store.js
+++ frontend/corso-app/src/redux/store.js
@@ -1,13 +1,15 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/authSlice';
 import customerReducer from '../features/customerSlice';
 import accountReducer from '../features/accountSlice';
 import transactionReducer from '../features/transactionSlice';
+import customerListReducer from '../features/customerListSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         customer: customerReducer,
+        customerList: customerListReducer,
         account: accountReducer,
         transaction: transactionReducer
     }
