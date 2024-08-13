a27e9b0212dbec6971bd8776348cd628804023ac
nhtY <nihat.yalcin@agu.edu.tr>
2024-08-13 12:16:48 +0300

    güncelle: pagination kütüphanesi eklendi. ListBrokerPage component adı düzenlendi

--- frontend/corso-app/src/pages/broker/ListBrokerPage.jsx
+++ frontend/corso-app/src/pages/broker/ListBrokerPage.jsx
@@ -4,7 +4,7 @@ import { userListBroker } from '../../features/userSlice';
 import { Table, Spinner, Alert } from 'react-bootstrap';
 import ReactPaginate from 'react-paginate';
 
-const BrokerListPage = () => {
+const ListBrokerPage = () => {
     const dispatch = useDispatch();
     const { userList, status, error } = useSelector(state => state.user);
 
@@ -94,4 +94,4 @@ const BrokerListPage = () => {
     );
 };
 
-export default BrokerListPage;
+export default ListBrokerPage;
