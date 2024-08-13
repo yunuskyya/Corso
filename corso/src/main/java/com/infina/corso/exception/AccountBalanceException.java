fe37a3de882dde4399ec7fe204ee2ec8a0b647fa
yunuskyya <0yunuskaya0@gmail.com>
2024-08-13 18:05:42 +0300

    AccountBalanceException: oluşturuldu.

---
+++ corso/src/main/java/com/infina/corso/exception/AccountBalanceException.java
@@ -0,0 +1,13 @@
+package com.infina.corso.exception;
+
+import com.infina.corso.shared.Messages;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AccountBalanceException extends RuntimeException {
+    public AccountBalanceException(Long accountId) {
+        super(Messages.getMessageForLocale("error.account.has.balance.cannot.delete", LocaleContextHolder.getLocale(), accountId));
+    }
+}
