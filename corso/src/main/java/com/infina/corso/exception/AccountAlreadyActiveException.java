129769008fe6ddb412daf82ceddd55c699c2df3e
yunuskyya <0yunuskaya0@gmail.com>
2024-08-13 18:05:23 +0300

    AccountAlreadyActiveException: oluşturuldu.

---
+++ corso/src/main/java/com/infina/corso/exception/AccountAlreadyActiveException.java
@@ -0,0 +1,13 @@
+package com.infina.corso.exception;
+
+import com.infina.corso.shared.Messages;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AccountAlreadyActiveException extends RuntimeException {
+    public AccountAlreadyActiveException(String accountNumber) {
+        super(Messages.getMessageForLocale("error.account.already.active", LocaleContextHolder.getLocale(), accountNumber));
+    }
+}
